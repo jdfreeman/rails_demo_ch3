f68bc5c824bf335efe8a386741ccf6a01f8c9bdd
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-06 18:41:08 +0100

    repec:install

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
+
+  describe "Home Page" do
+
+    it "should have content (Home)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+      visit "static_pages/home"
+      expect(page) .to have_content('Home')
     end
   end
 end
