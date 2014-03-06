b75c7da6152193d641adcb8275186c744d063037
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-06 16:34:03 -0600

    Full Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(title)
+    base_title = 'Ruby on Rails Tutorial'
+
+    unless title.empty?
+      base_title << " | #{title}"
+    end
+
+    base_title
 end
