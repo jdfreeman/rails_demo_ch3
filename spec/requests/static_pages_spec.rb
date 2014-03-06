24e6480db43d4b22c8c5d31382028368f1c2c0a3
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-06 19:30:32 +0100

    Title tag tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
-
     it "should have content (Home)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit "/static_pages/home"
       expect(page) .to have_content('Home')
+      expect(page) .to have_title('Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
@@ -15,6 +15,7 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit "/static_pages/help"
       expect(page) .to have_content('Help')
+      expect(page) .to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
   end
 
@@ -22,6 +23,7 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit "/static_pages/about"
       expect(page) .to have_content('About Us')
+      expect(page) .to have_title('Ruby on Rails Tutorial Sample App | About')
     end
   end
 end
