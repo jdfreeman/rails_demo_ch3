0408292bdb7bc8fa4b9dc065bbee7e81f6656fbe
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 08:43:19 -0600

    Refactor full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,7 @@ module ApplicationHelper
   def full_title(title)
     base_title = 'Ruby on Rails Tutorial'
 
-    unless title.empty?
-      base_title << " | #{title}"
-    end
+    base_title << " | #{title}" unless title.empty?
 
     base_title
   end
