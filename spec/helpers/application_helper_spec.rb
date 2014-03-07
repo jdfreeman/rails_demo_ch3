bb88b59d71018eeb92639d7182e026569cca8334
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 08:37:31 -0600

    full_title application helper testing

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,1 +1,19 @@
 require "spec_helper"
+
+describe ApplicationHelper do
+
+  describe 'full_title' do
+
+    it 'should include the page title' do
+      expect(full_title('foo')).to match(/foo/)
+    end
+
+    it 'should include the base title' do
+      expect(full_title('foo')).to match(/Ruby on Rails Tutorial/)
+    end
+
+    it 'should not include a page title on the home page' do
+      expect(full_title('')).not_to match (/\|/)
+    end
+  end
+end
