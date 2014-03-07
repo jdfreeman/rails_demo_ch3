b04d9b83a5232697d0bfe77c6ce6dedd3fa71a16
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 21:24:40 +0100

    Updated Email regex to pass tests; joe@example..com now fails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   validates :name,  presence: true,
                     length: {maximum: 255}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]{2,6}\z/i
   validates :email, presence: true,
                     format: {with: VALID_EMAIL_REGEX} ,
                     uniqueness: { case_senstive: false}
 end
-
