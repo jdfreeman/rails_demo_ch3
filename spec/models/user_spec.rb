b5a6029791714495da0495ff9e196c792bf35f32
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 13:02:23 -0600

    Failing tests for email validation 'a@b should fail'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,8 +25,25 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email is too long" do
-    before { @user.email = 'a' * 257 }
-    it { should_not be_valid }
+  describe "when email is malformed" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+
+      addresses.each do | invalid_address |
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email is wellformed" do
+    it "should be valid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+
+      addresses.each do | address |
+       @user.email = address
+       expect(@user).to be_valid
+      end
+    end
   end
 end
