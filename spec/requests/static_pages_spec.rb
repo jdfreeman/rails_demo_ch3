3cb8e60a74bb5ac32f0212d20d96ba4ebf2c310e
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-06 20:54:24 -0600

    Updated test to parameterize routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,49 +5,49 @@ describe "StaticPages" do
   describe "Home Page" do
     it "should have content 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/home"
+      visit home_path
       expect(page) .to have_content('home')
     end
 
     it "should have title 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/home"
+      visit home_path
       expect(page) .to_not have_title('Home')
     end
   end
 
   describe "Help Page" do
     it "should have the content 'Help'" do
-      visit "/static_pages/help"
+      visit help_path
       expect(page) .to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit "/static_pages/help"
+      visit help_path
       expect(page) .to have_title('Help')
     end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
-      visit "/static_pages/about"
+      visit about_path
       expect(page) .to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit "/static_pages/about"
+      visit about_path
       expect(page) .to have_title('About')
     end
   end
 
   describe "Contact Page" do
     it "should have the content 'Contact'" do
-      visit "/static_pages/contact"
+      visit contact_path
       expect(page) .to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit "/static_pages/contact"
+      visit contact_path
       expect(page) .to have_title('Contact')
     end
   end
