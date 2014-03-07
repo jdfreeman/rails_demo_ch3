156ab7422094e5ef723e95a1d78d0b6a88e890de
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 07:25:46 -0600

    New User Title

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 RailsDemoCh3::Application.routes.draw do
-  get "users/new"
   root 'static_pages#home'
-  match 'help', to: 'static_pages#help', via: 'get'
-  match 'about', to: 'static_pages#about', via: 'get'
-  match 'contact', to: 'static_pages#contact', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+
+  get 'users/new'  # Remove in 7.2
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
