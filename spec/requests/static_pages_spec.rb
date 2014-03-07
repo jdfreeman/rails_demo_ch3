74b8e49b206c5efda6a65bc015999ef164a76645
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-07 08:11:51 -0600

    Refactor tests for a shared_examples block

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,45 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for 'all static pages' do
+    it { should have_selector 'h1' , text: heading }
+    it { should have_title(page_title) }
+  end
+
   describe "Home Page" do
     before { visit root_path }
 
-    it { should have_content('home') }
-    it { should have_title('Ruby on Rails Tutorial') }
+    let (:heading) { 'Sample App'}
+    let (:page_title) { '' }
+
+    it_should_behave_like 'all static pages'
     it { should_not have_title('| Home') }
   end
 
   describe "Help Page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title('Help') }
+    let (:heading) { 'Help'}
+    let (:page_title) { 'Help' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe "About Page" do
     before { visit about_path }
 
-    it { should have_content('About Us') }
-    it { should have_title('About') }
+    let (:heading) { 'About'}
+    let (:page_title) { 'About' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe "Contact Page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title('Contact') }
+    let (:heading) { 'Contact'}
+    let (:page_title) { 'Contact' }
+
+    it_should_behave_like 'all static pages'
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,8 @@ describe "User pages" do
 
   describe "signup page" do
     before { visit signup_path }
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+
+    it { should have_selector 'h1', text: 'Sign Up' }
+    it { should have_title(full_title('Sign Up')) }
   end
 end
