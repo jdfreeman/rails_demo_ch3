264f50c5a36fe7e95799441e29e5b1edc3f2e37d
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-06 20:34:08 -0600

    Added Contact page test and corrected formatting

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
-    it "should have content (Home)" do
+    it "should have content 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit "/static_pages/home"
-      expect(page) .to have_content('Home')
-      expect(page) .to have_title('Home')
+      expect(page) .to have_content('home')
+    end
+
+    it "should have title 'Home'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit "/static_pages/home"
+      expect(page) .to_not have_title('Home')
     end
   end
 
@@ -15,6 +20,10 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit "/static_pages/help"
       expect(page) .to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit "/static_pages/help"
       expect(page) .to have_title('Help')
     end
   end
@@ -23,7 +32,23 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit "/static_pages/about"
       expect(page) .to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit "/static_pages/about"
       expect(page) .to have_title('About')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact Page'" do
+      visit "/static_pages/contact"
+      expect(page) .to have_content('Contact')
+    end
+
+    it "should have the title 'Contact Page'" do
+      visit "/static_pages/contact"
+      expect(page) .to have_title('Contact')
     end
   end
 end
