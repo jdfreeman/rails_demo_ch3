8f6b2de82be99ef9b3a46054c1fce1fa3ed0c859
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-10 23:23:37 +0100

    Added sessions routing to routes.rb 2

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ RailsDemoCh3::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
-
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
