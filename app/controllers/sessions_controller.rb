0bef973058fee9191eaa6c9d687d7eb376faec53
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-11 00:30:00 +0100

    Some authentication tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash[:error] = "Email or password invalid"
+      render 'new'
+    end
   end
 
   def destroy
