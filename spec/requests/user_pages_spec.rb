965639c1c9a653f41cf282e49763cf1ce9dacef6
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-10 23:09:48 +0100

    Updated flash testing and refactored flash messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,13 +11,17 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+
       it "should not create a user" do
+
         expect { click_button submit }.not_to change(User, :count)
       end
     end
 
     describe "with valid information" do
+
       before do
+
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
@@ -25,7 +29,17 @@ describe "User pages" do
       end
 
       it "should create a user" do
+
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving user" do
+
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
       end
     end
   end
