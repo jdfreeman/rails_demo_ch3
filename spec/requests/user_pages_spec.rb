1c8bcf3f2ed4916e06aa1bcb3bf063bd29b818c9
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-10 21:08:02 +0100

    Source checkin, tests still not working

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,12 +18,17 @@ describe "User pages" do
 
     describe 'with valid information' do
 
-      before do
-        fill_in 'Name',         with: "Example User"
-        fill_in 'Email',        with: 'user@example.com'
-        fill_in 'Password',     with: 'foobar'
-        fill_in 'Confirmation', with: 'foobar'
+      # let (:valid_user) { FactoryGirl.create(:user) }
 
+      # it "should be a valid user" do
+      #   expect(valid_user.save).to change(User, :count).by(1)
+      # end
+
+      before do
+        fill_in 'Name', with: "Example User"
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'foobar'
+        fill_in 'Confirmation', with: 'foiaobar'
       end
 
       it 'should create a user' do
