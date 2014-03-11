38586828cf162eb1d6d43120bd1a71d0d8bcdb44
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-11 08:59:02 -0500

    Signin upon signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,7 @@ describe "User pages" do
 
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+        it { should have_link('Sign out', href: signout_path) }
       end
     end
   end
