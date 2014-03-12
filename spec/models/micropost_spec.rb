fe398021bc1991342c05d074634a85129c5d42da
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-12 23:50:42 +0100

    Microposts now respond to content and user_id attributes

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  it "should respond to the :content attribute" do
+    expect(@micropost).to respond_to(:content)
+  end
+
+  it "should respond to the :user_id attribute" do
+    expect(@micropost).to respond_to(:user_id)
+  end
 end
