f2ff1139acfdd4302e5dd7a96ea91d2e5825fada
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-12 08:15:16 -0500

    Administrative tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,13 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:name) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
+  it { should respond_to(:name) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:password_digest) }
   it { should respond_to(:remember_token) }
 
   it { should be_valid }
@@ -122,4 +123,14 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 
+  describe "with admin set to true" do
+    before do
+      @user.save!
+      @user.toggle(:admin)
+    end
+
+    it "should be an admin" do
+      expect(@user).to be_admin
+    end
+  end
 end
