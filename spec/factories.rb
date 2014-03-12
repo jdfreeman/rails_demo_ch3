21dad9ecfdb53f0f8def06a0ffab7b8a7a286d90
Josh Freeman <jdfreeman@satx.rr.com>
2014-03-12 07:57:35 -0500

    FactoryGirl sequences

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name 'Michael Hartl'
-    email 'hartl@example.com'
-    password 'foobar'
-    password_confirmation 'foobar'
-  end
-
-  factory :user_gravitar do
-    name 'Michael Hartl'
-    email 'example@railstutorial.com'
-    gravitar_src "https://secure.gravatar.com/avatar/f1a6a90ea0fceff55be9e5abf44f9a1e"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
